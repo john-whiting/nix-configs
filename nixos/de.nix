4bde6badddce1de5254c49f4373ed3cdd0ab3189
John Whiting <john@jwhiting.dev>
2026-01-19 17:07:13 -0500

    feat(nixos): switch to cosmic de

--- nixos/de.nix
+++ nixos/de.nix
@@ -1,10 +1,19 @@
 {
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # Enable the COSMIC login manager
+  services.displayManager.cosmic-greeter.enable = true;
 
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  # Enable the COSMIC desktop environment
+  services.desktopManager.cosmic.enable = true;
+
+  # Use system76's scheduler to improve performance
+  services.system76-scheduler.enable = true;
+
+  # Allow the clipboard to work between applications
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+
+  programs.firefox.preferences = {
+    # disable libadwaita theming for Firefox
+    # this let's it pick up on Cosmic's theme
+    "widget.gtk.libadwaita-colors.enabled" = false;
   };
 }
