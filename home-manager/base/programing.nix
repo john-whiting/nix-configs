7647e6f27e1460fd468da39a64bf79dd95ce1bee
John Whiting <john@jwhiting.dev>
2026-04-07 21:40:41 -0400

    feat: added devenv to programming module

--- home-manager/base/programing.nix
+++ home-manager/base/programing.nix
@@ -9,6 +9,7 @@
   ];
 
   home.packages = with pkgs; [
+    devenv
     markdownlint-cli2 # needed for markdown files in neovim
     (writeShellScriptBin "markdownlint" ''
       exec ${markdownlint-cli2}/bin/markdownlint-cli2 "$@"
