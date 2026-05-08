de97cbe776bbb645dbecce047a8c5510e02c53b7
John Whiting <john@jwhiting.dev>
2026-05-08 09:40:18 -0400

    feat(zsh): added better error handling and service name discovery for devc

--- home-manager/base/programs/zsh.nix
+++ home-manager/base/programs/zsh.nix
@@ -72,18 +72,46 @@
 
         devc () {
           compose_file=$(find .devcontainer -regextype posix-extended -regex '.*\/(docker-)?compose.ya?ml')
+
+          if [[ -z $compose_file ]]
+          then
+            echo "Unable to find devcontainer compose file." >&2
+            return 1
+          fi
+
           compose_file_data=$(docker compose -f "$compose_file" config 2> /dev/null)
+
+          # Strip some comments from the JSON file
+          # Your mileage may vary.
+          devcontainer_json_data=$(cat .devcontainer/devcontainer.json | awk '{ if ($0 ~ /^\s*\/\//) { next }; gsub(/\/\/.*$/, ""); print }')
 
           if [[ "$1" = "code" ]]
           then
             container_name="$2"
             if [[ -z "$container_name" ]]
             then
-              # Strip some comments from the JSON file, then get service.
-              # Your mileage may vary.
-              service_name=$(cat .devcontainer/devcontainer.json | awk '{ if ($0 ~ /^\s*\/\//) { next }; gsub(/\/\/.*$/, ""); print }' | yq '.service' -r)
+              service_name=$(echo "$devcontainer_json_data" | yq '.service' -r)
+              if [[ -z "$service_name" || "$service_name" = "null" ]]
+              then
+                echo "Unable to find service in devcontainer.json." >&2
+                return 2
+              fi
+
+              project_name=$(echo "$compose_file_data" | yq ".name" -r)
+              if [[ -z "$project_name" || "$project_name" = "null" ]]
+              then
+                project_name=""
+              else
+                project_name="$project_name-"
+              fi
+
               container_name=$(echo "$compose_file_data" | yq ".services[\"$service_name\"].container_name" -r)
-              echo "Found service '$service_name' with container name '$container_name'"
+              if [[ -z "$container_name" || "$container_name" = "null" ]]
+              then
+                container_name="$project_name$service_name-1"
+              fi
+
+              echo "Found service '$service_name' with container name '$container_name'" >&2
             fi
             container_state=$(docker ps -a --filter name="$container_name" --format=json | yq '.State' -r)
             if [[ -z "$container_state" ]]
@@ -97,13 +125,13 @@
               fi
             elif [[ "$container_state" = "exited" ]]
             then
-              echo "Restarting stopped containers..."
+              echo "Restarting stopped containers..." >&2
               docker compose -f "$compose_file" start
             fi
             container_json="$(echo -n "{\"containerName\":\"$container_name\"}" | xxd -ps -c 128)"
             workspace_path=''${3:="workspace"}
             folder_uri="vscode-remote://attached-container+$container_json/$workspace_path"
-            echo "Attaching to '$folder_uri'..."
+            echo "Attaching to '$folder_uri'..." >&2
             code --folder-uri=$folder_uri
           else
             docker compose -f "$compose_file" $@
