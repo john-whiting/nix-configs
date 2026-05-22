7390266efe6623d8d37eedff7a3d5caed84f908f
John Whiting <john@jwhiting.dev>
2026-05-22 11:35:54 -0400

    feat: added zsh loading of nix single-user file

--- home-manager/base/programs/zsh.nix
+++ home-manager/base/programs/zsh.nix
@@ -141,6 +141,10 @@
         kubectx() {
           kubectl config use-context $(kubectl config get-contexts -o name | fzy)
         }
+
+        if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+          . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+        fi
       '';
     };
   };
