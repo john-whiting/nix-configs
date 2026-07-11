c6b1675d47255552596f9a2844cc2cd5554328e0
John Whiting <john@jwhiting.dev>
2026-07-11 11:26:38 -0400

    os: added steam to base

--- nixos/apps.nix
+++ nixos/apps.nix
@@ -27,4 +27,27 @@ in
 
   virtualisation.docker.enable = true;
   systemd.services.docker.wantedBy = lib.mkForce [ ];
+
+  # Gaming
+  programs.gamemode.enable = true;
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers   enable = true;
+    gamescopeSession.enable = true;
+  };
+
+  # NOTE: https://github.com/NixOS/nixpkgs/issues/523200
+  security.wrappers.bwrap = lib.mkForce {
+    source = "${pkgs.bubblewrap}/bin/bwrap";
+    owner = "root";
+    group = "root";
+    setuid = false;
+    setgid = false;
+  };
 }
